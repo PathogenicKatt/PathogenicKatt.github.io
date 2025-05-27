860dbe3fcf9db3fb82cc17979c37be4d9c26d6cf
Katleho Mofokeng <katlehom814@gmail.com>
2025-05-27 15:34:40 +0200

    Trying to find the solution for the CV

--- hash.js
+++
@@ -1,8 +0,0 @@
-async function generateHash() {
-  const encoder = new TextEncoder();
-  const data = encoder.encode("indabaX@katleho001 ");
-  const hash = await crypto.subtle.digest('SHA-256', data);
-  const hashArray = Array.from(new Uint8Array(hash));
-  console.log("Your hash:", hashArray.map(b => b.toString(16).padStart(2, '0')).join(''));
-}
-generateHash();
